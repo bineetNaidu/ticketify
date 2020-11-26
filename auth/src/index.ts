18a92e620a2931bef98d91eb187505cf35a1985b
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-26 22:23:11 +0530

    intregated ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => res.send('Hello World'));
+
 app.listen(3000, () =>
   console.log('>>>> Auth Service Started | PORT: 3000 <<<<'),
 );
