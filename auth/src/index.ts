264a2f6ab60c0e476dd25c10fd5bf5feaade66d6
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-27 14:26:58 +0530

    intregated the base auth routes to index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 
+//? Routers
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => res.send('Hello World'));
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () =>
   console.log('>>>> Auth Service Started | PORT: 3000 <<<<'),
