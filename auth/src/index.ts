15d5e98795fa61128e9a35f29dff44ad7a87b1f7
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-27 22:09:37 +0530

    add cookie session suport to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,17 +3,24 @@ import 'express-async-errors';
 import mongoose from 'mongoose';
 import errorHandler from './middlewares/errorHandler';
 import NotFoundError from './errors/NotFoundError';
+import cookieSession from 'cookie-session';
 
 //? Routers
 import { currentUserRouter } from './routes/currentUser';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 const app = express();
 
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
