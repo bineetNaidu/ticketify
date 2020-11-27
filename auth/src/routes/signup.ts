7cc997be652ac33f88df564a532a22a6154660ea
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-27 14:48:18 +0530

    setting up for signup auth routes with validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,30 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.post('/api/users/signup', async (req, res) => res.send('Hello world'));
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 6, max: 24 })
+      .withMessage('Password must be between 6 and 20 Characters'),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+      }
+
+      const { email, password } = req.body;
+      console.log('Creating a user....');
+      res.status(201).json({ status: 'OK' });
+    } catch (e) {}
+  },
+);
 
 export { router as signupRouter };
