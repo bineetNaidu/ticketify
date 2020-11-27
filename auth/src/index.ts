9c9e033781bb3573ade6c91dd246e42e0a7ea59b
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-27 17:51:59 +0530

    -- connecting to mongo k8s service -- updated auth-mongo-depl.yaml

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import errorHandler from './middlewares/errorHandler';
 import NotFoundError from './errors/NotFoundError';
 
@@ -8,6 +9,7 @@ import { currentUserRouter } from './routes/currentUser';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 const app = express();
 
@@ -26,6 +28,21 @@ app.all('*', () => {
 // ! Error Handlers
 app.use(errorHandler);
 
-app.listen(3000, () =>
-  console.log('>>>> Auth Service Started | PORT: 3000 <<<<'),
-);
+const startAuthServiceWithMongoDB = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('>> Auth MONGO DB : Connected');
+  } catch (e) {
+    console.error(e.message);
+  }
+
+  app.listen(3000, () =>
+    console.log('>>>> Auth Service Started | PORT: 3000 <<<<'),
+  );
+};
+
+startAuthServiceWithMongoDB();
