017e9d8a10d91fdc403e5751f868087f317abf81
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-29 00:18:30 +0530

    setting up signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,24 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
+import RequestValidationError from '../errors/RequestValidatorError';
 
 const router = Router();
 
-router.post('/api/users/signin', async (req, res) => res.send('Hello world'));
-
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be Valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must provide a Password!'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    res.send('OK');
+  },
+);
 export { router as signinRouter };
