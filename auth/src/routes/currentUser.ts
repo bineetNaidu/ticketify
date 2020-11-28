e1881a57fd44ae07d3731c3565faa14e76cf475f
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-29 00:50:58 +0530

    seeting up currentUser route

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-router.get('/api/users/currentuser', async (req, res) =>
-  res.send('Hello world'),
-);
+router.get('/api/users/currentuser', async (req, res) => {
+  if (!req.session?.ticketifyJwt) {
+    return res.json({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.ticketifyJwt, process.env.JWT_KEY!);
+    res.json({ currentUser: payload });
+  } catch (e) {
+    res.json({ currentUser: null });
+  }
+});
 
 export { router as currentUserRouter };
