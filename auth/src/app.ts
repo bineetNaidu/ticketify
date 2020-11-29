20d33034d62d47dec0927ceb74017bc762231a5a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-29 14:50:31 +0530

    #setting up for test -- add dev tests dependencies -- updated Dockerfile -- Index.ts to app.ts refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import errorHandler from './middlewares/errorHandler';
+import NotFoundError from './errors/NotFoundError';
+import cookieSession from 'cookie-session';
+import 'express-async-errors';
+
+//? Routers
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+//! Not found page error
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+// ! Error Handlers
+app.use(errorHandler);
+
+export default app;
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import errorHandler from './middlewares/errorHandler';
-import NotFoundError from './errors/NotFoundError';
-import cookieSession from 'cookie-session';
-
-//? Routers
-import { currentUserRouter } from './routes/currentUser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-const app = express();
-
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  }),
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-//! Not found page error
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-// ! Error Handlers
-app.use(errorHandler);
+import app from './app';
 
 const startAuthServiceWithMongoDB = async () => {
   if (!process.env.JWT_KEY) {
