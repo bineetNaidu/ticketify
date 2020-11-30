90317f8c1a50f721d6feb61610c5fde439629b97
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-30 21:03:38 +0530

    add signup handlers

--- client/pages/auth/signup.jsx
+++ client/pages/auth/signup.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/useRequest';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
   return (
-    <form className="container">
+    <form className="container" onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+      {errors}
       <button type="submit" className="btn btn-primary">
         Sign up
       </button>
