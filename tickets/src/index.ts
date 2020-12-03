dd04d49cf8eb55ffa54359201a9314ff71aa1728
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-03 13:00:08 +0530

    add mongo uri as ENV

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 import app from './app';
 
-const startAuthServiceWithMongoDB = async () => {
+const startTicketsServiceWithMongoDB = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!!');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined!!');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('>> Auth MONGO DB : Connected');
+    console.log('>> Tickets MONGO DB : Connected');
   } catch (e) {
     console.error(e.message);
   }
 
   app.listen(3000, () =>
-    console.log('>>>> Auth Service Started | PORT: 3000 <<<<'),
+    console.log('>>>> Tickets Service Started | PORT: 3000 <<<<'),
   );
 };
 
-startAuthServiceWithMongoDB();
+startTicketsServiceWithMongoDB();
