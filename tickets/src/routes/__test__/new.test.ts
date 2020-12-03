924cebdda050feaf65e389d72f31e4ec76de5bf0
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-03 16:42:47 +0530

    bumped @bnticketify/common version

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -6,7 +6,9 @@ it('has a route handler listening to /api/tickets for post requests', async () =
   expect(res.status).not.toEqual(404);
 });
 
-it('can only be accessed if the user is signed in', async () => {});
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post('/api/tickets').send({}).expect(401);
+});
 
 it('returns an error if an invalid title is provided', async () => {});
 
