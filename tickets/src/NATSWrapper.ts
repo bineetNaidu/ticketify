168a5c62d9152d617613a06e8b1b7f2efcd6401c
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-10 23:21:56 +0530

    add NATSWrapper class

---
+++ tickets/src/NATSWrapper.ts
@@ -0,0 +1,22 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NATSWrapper {
+  private _client?: Stan;
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this._client?.on('connect', () => {
+        console.log('Connected to NATS...');
+        resolve();
+      });
+
+      this._client?.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NATSWrapper();
