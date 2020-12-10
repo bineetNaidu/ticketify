f74248076c39f84075ae7b672eaa0d44174ba7a3
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-10 23:34:50 +0530

    add access to NATs client

--- tickets/src/NATSWrapper.ts
+++ tickets/src/NATSWrapper.ts
@@ -3,16 +3,23 @@ import nats, { Stan } from 'node-nats-streaming';
 class NATSWrapper {
   private _client?: Stan;
 
+  public get client(): Stan {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+    return this._client;
+  }
+
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
-    return new Promise((resolve, reject) => {
-      this._client?.on('connect', () => {
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS...');
         resolve();
       });
 
-      this._client?.on('error', (err) => {
+      this.client.on('error', (err) => {
         reject(err);
       });
     });
