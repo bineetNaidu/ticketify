1b7af75e46c74e02d1a9461832c950d164a5a949
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-26 23:38:12 +0530

    add index route handler for order service

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,8 +1,15 @@
 import { Request, Response, Router } from 'express';
+import { requireAuth } from '@bnticketify/commons';
+import Order from '../models/Order';
+
 const router = Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  res.json({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser?.id,
+  }).populate('ticket');
+
+  res.json(orders);
 });
 
 export { router as indexOrderRoute };
