0bdbcc31e1c1affc3cdc131bd4dc727d90b50c2f
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-26 23:21:02 +0530

    add new route order service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,56 @@
 import { Request, Response, Router } from 'express';
-const router = Router();
+import mongoose from 'mongoose';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@bnticketify/commons';
+import { body } from 'express-validator';
+import Order from '../models/Order';
+import Ticket from '../models/Ticket';
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.json({});
-});
+const router = Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60; //? 15 mins
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    //*  Find the ticket the user is trying to order in the DB
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    //*  Make sure the ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('The Ticket is reserved!');
+    }
+    //* Calculate an expiration date/time for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    //* Build the order and save it to the DB
+    const order = await Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    //* Emmit / Publish a Event
+
+    res.status(201).json(order);
+  },
+);
 
 export { router as newOrderRoute };
