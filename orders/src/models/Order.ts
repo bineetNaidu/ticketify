832218998c01aa88b9a896fb9aea65b9df6f1c95
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-26 22:50:47 +0530

    add Order model for orders service

---
+++ orders/src/models/Order.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@bnticketify/commons';
+import { TicketDoc } from './Ticket';
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  id: string;
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      versionKey: false,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => new Order(attrs);
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', OrderSchema);
+
+export default Order;
