a94d3c64ae73880523fb338356a75632683490f2
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-27 12:01:30 +0530

    add show route for orders service

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,8 +1,25 @@
 import { Request, Response, Router } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@bnticketify/commons';
+import Order from '../models/Order';
 const router = Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.json({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    res.json(order);
+  },
+);
 
 export { router as showOrderRoute };
