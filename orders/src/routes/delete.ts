c5d63c00324bc37e8951352e42d800e0797aa372
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-27 12:40:42 +0530

    add delete or patch route for order service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,8 +1,34 @@
 import { Request, Response, Router } from 'express';
+import {
+  requireAuth,
+  OrderStatus,
+  NotAuthorizedError,
+  NotFoundError,
+} from '@bnticketify/commons';
+import Order from '../models/Order';
 const router = Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.json({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    //! publish an event saying this was cancelled
+
+    res.status(204).json(order);
+  },
+);
 
 export { router as deleteOrderRoute };
