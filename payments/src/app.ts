f9a65da941db63a4e1ebb5405b4f6a8cbb7177b4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-01 21:06:03 +0530

    intial setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { errorHandler, NotFoundError, currentUser } from '@bnticketify/commons';
+import cookieSession from 'cookie-session';
+import 'express-async-errors';
+
+//? Routers
+
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+app.use(currentUser);
+
+//! Not found page error
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+// ! Error Handlers
+app.use(errorHandler);
+
+export default app;
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,60 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+jest.mock('../NATSWrapper');
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'qwerty';
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+  // await mongoose.connection.dropDatabase();
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  //?  Build a JWT payload
+  const payload = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+  //? Create a JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  //? Build a session Object { ticketifyJwt: MY_JWT_OBJ }
+  const session = { ticketifyJwt: token };
+  //? Turn Session into JSON
+  const sessJson = JSON.stringify(session);
+  //? Take JSON and encode as base64
+  const base64 = Buffer.from(sessJson).toString('base64');
+  //? return cookie and base64 ecodded data
+  return [`express:sess=${base64}`];
+};
